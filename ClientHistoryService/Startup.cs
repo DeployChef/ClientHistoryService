6205e9356ce73e28ba8ed9f9d28e6a5a528ebbb7
DeployChef <dc.titovvlad@gmail.com>
2020-10-14 20:39:10 +0300

    Подключил все это добро к постгресу

--- ClientHistoryService/Startup.cs
+++ ClientHistoryService/Startup.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ClientHistoryService.Domain.Interfaces;
+using ClientHistoryService.Infrastructure.Db;
 using ClientHistoryService.Infrastructure.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -42,6 +44,9 @@ namespace ClientHistoryService
             
             services.AddScoped<IClientHistoryService, Domain.Services.ClientHistoryService>();
             services.AddScoped<IClientHistoryRepository, ClientHistoryRepository>();
+
+            services.AddDbContext<ClientHistoryDbContext>(options =>
+                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
